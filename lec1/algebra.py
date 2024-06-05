ae5f82d429467d8a3dbf0652b208dffb19034bb8
Kaoru Kato <kaoru.kato.2003@icloud.com>
2024-06-05 09:43:00 +0900

    a

---
+++ lec1/algebra.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+第1回演習問題
+"""
+import numpy as np
+
+########## 課題1(a)
+print("Hello World!")
+
+########## 課題1(b)i
+
+# print("A = ", A) # 表示方法の例
+# print("b = ", b)
+
+########## 課題1(b)ii
+
+
+########## 課題1(b)iii
+
+
+########## 課題1(c) 例
+print("--- Kadai 1(c) example ---")
+x = 0
+for n in range(1, 11): # この指定でnは1から10まで動く
+    x = 2*x + 1
+    print("a(", n, ") =", x)
+    
+########## 課題1(c)
