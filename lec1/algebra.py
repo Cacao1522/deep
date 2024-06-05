cd93fd56a7712bcc0faf63cded3598ef4e4b7ce3
Kaoru Kato <kaoru.kato.2003@icloud.com>
2024-06-05 11:06:10 +0900

    a

--- lec1/algebra.py
+++ lec1/algebra.py
@@ -9,31 +9,31 @@ import numpy as np
 print("Hello World!")
 
 ########## 課題1(b)i
-A = np.reshape(np.array(range(1, 21), dtype = float),newshape = (4, 5))
-b = np.array([1,0,1,0,1], dtype = float)
-print("A = ", A) # 表示方法の例
+A = np.reshape(np.array(range(1, 21), dtype=float), newshape=(4, 5))
+b = np.array([1, 0, 1, 0, 1], dtype=float)
+print("A = ", A)  # 表示方法の例
 print("b = ", b)
 
 ########## 課題1(b)ii
-Ab = np.dot(A,b)
+Ab = np.dot(A, b)
 print("Ab = ", Ab)
 ########## 課題1(b)iii
-sum_column = np.sum(A,axis=0)
-sum_line =np.sum(A,axis=1)
+sum_column = np.sum(A, axis=0)
+sum_line = np.sum(A, axis=1)
 print("列和：", sum_column)
 print("行和：", sum_line)
 ########## 課題1(c) 例
 print("--- Kadai 1(c) example ---")
 x = 0
-for n in range(1, 11): # この指定でnは1から10まで動く
-    x = 2*x + 1
+for n in range(1, 11):  # この指定でnは1から10まで動く
+    x = 2 * x + 1
     print("a(", n, ") =", x)
-    
+
 ########## 課題1(c)
-x=6
-for n in range(1,11):
-    if x%2==0:
-        x/=2
+x = 6
+for n in range(1, 11):
+    if x % 2 == 0:
+        x /= 2
     else:
-        x=3*x+1
+        x = 3 * x + 1
     print("a(", n, ") =", x)
--- lec1/perceptron.py
+++ lec1/perceptron.py
@@ -5,24 +5,25 @@
 """
 import numpy as np
 import matplotlib as mpl
-mpl.use('Agg')
+
+mpl.use("Agg")
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
 
-use_small_data = True # False
+use_small_data = False
 
 ##### データの取得 (MNIST 手書き数字データセット)
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # 学習データ
-x_train = x_train.astype('float32') / 255.
-x_train = x_train.reshape([60000, 28*28])
-x_train = x_train[y_train <= 1,:]
+x_train = x_train.astype("float32") / 255.0
+x_train = x_train.reshape([60000, 28 * 28])
+x_train = x_train[y_train <= 1, :]
 
 # テストデータ
-x_test = x_test.astype('float32') / 255.
-x_test = x_test.reshape([10000, 28*28])
-x_test = x_test[y_test <= 1,:]
+x_test = x_test.astype("float32") / 255.0
+x_test = x_test.reshape([10000, 28 * 28])
+x_test = x_test[y_test <= 1, :]
 
 y_train = np.array(y_train[y_train <= 1], float)
 y_test = np.array(y_test[y_test <= 1], float)
@@ -31,7 +32,7 @@ if use_small_data:
     ## プログラム作成中は訓練データを小さくして，
     ## 実行時間が短くようにしておく
     y_train = y_train[range(1000)]
-    x_train = x_train[range(1000),:]
+    x_train = x_train[range(1000), :]
 
 n, d = x_train.shape
 n_test, _ = x_test.shape
@@ -44,19 +45,22 @@ n_test, _ = x_test.shape
 
 np.random.seed(123)
 
+
 ##### シグモイド関数, 誤差関数
 def sigmoid(x):
-    # 課題2(a) 
+    # 課題2(a)
     # returnの後にシグモイド関数を返すプログラムを書く
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
 
 def error_function(f, y):
-    # 課題2(b) 
+    # 課題2(b)
     # returnの後に誤差関数を返すプログラムを書く
-    return -y*np.log(f)-(1-y)*log(1-f)
+    return -y * np.log(f) - (1 - y) * np.log(1 - f)
+
 
 ##### パラメータの初期値 (ランダム)
-w = np.random.normal(0, 0.3, d+1)
+w = np.random.normal(0, 0.3, d + 1)
 
 ########## 確率的勾配降下法によるパラメータ推定
 error = []
@@ -69,20 +73,20 @@ eta = 0.01
 for epoch in range(0, num_epoch):
     index = np.random.permutation(n)
 
-    e_train = np.full(n,np.nan) # 誤差保存用のサイズnの配列をNaNで初期化
+    e_train = np.full(n, np.nan)  # 誤差保存用のサイズnの配列をNaNで初期化
     for i in index:
-        
-        xi = np.append(1, x_train[i, :]) # 特徴ベクトルに定数1を結合したものを作成
+
+        xi = np.append(1, x_train[i, :])  # 特徴ベクトルに定数1を結合したものを作成
         yi = y_train[i]
 
-        ### 課題2(a) 
+        ### 課題2(a)
         # パーセプトロンの出力f(w^T x)を計算する (活性化関数fはシグモイド)
-        # ヒント: 先に上でsigmoid関数を定義し, 
+        # ヒント: 先に上でsigmoid関数を定義し,
         #         パラメータ変数'w'と入力'xi'の内積(np.dot)の結果を渡す
         # fi = シグモイドの出力 (i番目のx(xi)に対するfなのでfiとしてある)
-        fi = np.dot(sigmoid(w,xi))
+        fi = sigmoid(np.dot(w, xi))
 
-        ### 課題2(b) 誤差評価 
+        ### 課題2(b) 誤差評価
         # fを計算したら，error_functionを定義し以下のコメントを外す
         e_train[i] = error_function(fi, yi)
 
@@ -95,39 +99,36 @@ for epoch in range(0, num_epoch):
         ### 課題2(c)
         # パラメータの更新
         # 確率的勾配降下法の更新式を書く（学習率eta_tの設定に注意）
-        w = w-eta*(fi-yi)@xi
-    
+        w = w - eta * (fi - yi) * xi
+
     ##### エポックごとの訓練誤差
-    error.append(sum(e_train)/n)
-    
+    error.append(sum(e_train) / n)
+
     ##### テスト誤差
-    e_test = np.full(n_test, np.nan) 
-    for j in range(0, n_test):        
-        
+    e_test = np.full(n_test, np.nan)
+    for j in range(0, n_test):
+
         xi = np.append(1, x_test[j, :])
         yi = y_test[j]
 
-        ### 課題2(a) 
+        ### 課題2(a)
         # パーセプトロンの出力fを計算する(テスト)
-        fi = np.dot(sigmoid(w,xi))
+        fi = sigmoid(np.dot(w, xi))
 
-        ### 課題2(b) 誤差評価 
-        # fを計算したら，error_functionを定義し以下のコメントを外す 
+        ### 課題2(b) 誤差評価
+        # fを計算したら，error_functionを定義し以下のコメントを外す
         e_test[j] = error_function(fi, yi)
-    
-    error_test.append(sum(e_test)/n_test)
+
+    error_test.append(sum(e_test) / n_test)
 
 ########## 誤差関数のプロット
 plt.figure()
-plt.plot(error, label="training", lw=3)     #青線
-plt.plot(error_test, label="test", lw=3)     #オレンジ線
-plt.yscale('log')
+plt.plot(error, label="training", lw=3)  # 青線
+plt.plot(error_test, label="test", lw=3)  # オレンジ線
+plt.yscale("log")
 plt.grid()
 plt.ylabel("Error function (log scale)")
 plt.xlabel("Epoch")
-plt.legend(fontsize =16)
-plt.savefig("./error.pdf", bbox_inches='tight', transparent=True)
+plt.legend(fontsize=16)
+plt.savefig("./error.pdf", bbox_inches="tight", transparent=True)
 plt.close()
-
-
-
